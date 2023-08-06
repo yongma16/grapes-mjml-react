d3f9fee33139d48e5203ff622b7caf0e966886c5
yma16 <17685043634@163.com>
2023-08-06 18:57:57 +0800

    feat(log): log 日志记录，chrome 和 edg的渲染不同的bug导致组件无法拖拽

--- src/views/EmailPage.tsx
+++ src/views/EmailPage.tsx
@@ -1,63 +1,25 @@
-import grapesJSMJML  from '../components/email-edit'
+import grapesJSMJML  from '../components/email-edit/index'
 import { useEffect, useState } from 'react'
+
+
 const EmailPage=(props:any)=>{
     const [editor,setEditor]=useState();
     const [domRef,setDomRef]=useState();
-
-
-
-    const renderGrape = () => {
-        if(domRef&&!editor){
-            // @ts-ignore
-            const editorInstance:any = props.editInstance
-                .init({
-                    fromElement: true,
-                    container: '#gjs-email',
-                    plugins: [grapesJSMJML ],
-                    // pluginsOpts: {
-                    //     [grapesJSMJML]: {/* ...options */}
-                    // },
-                });
-            const baseTemplate= `<mjml>
+    const [mjmlTemplate,setMjmlTemplate]=useState(`<mjml>
                                     <mj-body>
                                       <mj-section>
                                       </mj-section>
                                     </mj-body>
-                                  </mjml>`;
-            editorInstance.addComponents(baseTemplate);
-            console.log('editorInstance',editorInstance)
-            console.log('editorInstance.addComponents',editorInstance.addComponents)
-            console.log('grapesJSMJML',grapesJSMJML)
-            setEditor(editorInstance)
-        }
-    }
-    //
-    // useEffect(()=>{
-    //     renderGrape()
-    // },[renderGrape])
-
+                                  </mjml>`)
     useEffect(()=>{
-
-        const baseTemplate= `<mjml>
-                                    <mj-body>
-                                      <mj-section>
-                                      </mj-section>
-                                    </mj-body>
-                                  </mjml>`;
         const editorInstance:any = props.editInstance
             .init({
                 fromElement: true,
                 container: '#gjs-email',
-                components:baseTemplate,
                 plugins: [grapesJSMJML ],
-                // pluginsOpts: {
-                //     [grapesJSMJML]: {/* ...options */}
-                // },
             });
-        editorInstance.addComponents(baseTemplate);
-        console.log('editorInstance',editorInstance)
-        console.log('editorInstance.addComponents',editorInstance.addComponents)
-        console.log('grapesJSMJML',grapesJSMJML)
+        // editorInstance.addComponents(mjmlTemplate);
+        console.log('getHtml',editorInstance.getHtml())
         setEditor(editorInstance)
     },[])
 
