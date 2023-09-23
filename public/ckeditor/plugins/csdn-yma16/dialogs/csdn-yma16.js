12bf6c8dba99d80153559ae9688be6e2f1271b65
yma16 <17685043634@163.com>
2023-09-24 06:33:30 +0800

    feat: style

--- public/ckeditor/plugins/csdn-yma16/dialogs/csdn-yma16.js
+++ public/ckeditor/plugins/csdn-yma16/dialogs/csdn-yma16.js
@@ -9,44 +9,75 @@ CKEDITOR.dialog.add('csdn-yma16', function (editor) { //要和plugin.js 中的co
         contents: [{   //对话框内容
             id: 'choice',
             name: 'choice',
-            label: '插入选择题答案',
-            title: '插入选择题答案',
+            label: 'radio 单项 选项',
+            title: 'radio 单项 选项',
             elements: [{
                 id: 'yma16_1',
                 type: 'radio', //表单元素类型：单选按钮
-                items: [['csdn','[博客]'],['juejin','[博客]']]
-            },
-                {
-                    id: 'yma16_2',
-                    type: 'radio', //表单元素类型：单选按钮
-                    items: [['huawei','[芯片]'],['apple','[美国芯片]']]
+                items: [['csdn','[博客]']
+                    ,['huawei','[华为云]']
+                    ,['aliyun','[阿里云]']
+                    ,['juejin','[掘金]']]
+            }]
+        },{
+            id: 'info',
+            label: '输入框',
+            title: '输入框',
+            elements: [ {
+                type: 'text',
+                id: 'linkDisplayText',
+                label: '链接跳转',
+                setup: function() {
+                    this.enable();
+
+                    this.setValue( editor.getSelection().getSelectedText() );
+
+                    // Keep inner text so that it can be compared in commit function. By obtaining value from getData()
+                    // we get value stripped from new line chars which is important when comparing the value later on.
+                    initialLinkText = this.getValue();
                 },
-                {
-                    id: 'yma16_3',
-                    type: 'radio', //表单元素类型：单选按钮
-                    items: [['name','[姓名]']]
+                commit: function( data ) {
+                    data.linkText = this.isEnabled() ? this.getValue() : '';
+                }
+            }]},
+            {
+                id: 'info2',
+                label: '选择框',
+                title: '选择框',
+                elements: [ {
+                    id: 'linkDisplayText',
+                    type: 'select',
+                    width:'500px',
+                    label: '选择',
+                    'default': 'url',
+                    items: [
+                        [ 'url', 'url' ],
+                        [ 'anchor', 'anchor' ],
+                        [ 'email', 'email' ],
+                        [ 'tel', 'tel' ]
+                    ],
+                    onChange: function(val){
+                        console.log('val',val)
+                },
+                    setup: function( data ) {
+                        this.setValue( data.type || 'url' );
+                    },
+                    commit: function( data ) {
+                        data.type = this.getValue();
+                    }
                 }]
-        }],
+            }
+        ],
         onOk: function () { //点击确定按钮出发onOK事件。以下代码主要目的是构造一个select下拉框
             let a = this.getValueOf('choice', 'yma16_1');
-            let b = this.getValueOf('choice', 'yma16_2');
-            let c = this.getValueOf('choice', 'yma16_3');
             let rtn = "";
 
             if(a != null){
                 rtn += a;
             }
 
-            if(b != null){
-                rtn += b;
-            }
-
-            if(c != null){
-                rtn += c;
-            }
-
-            if (rtn != "") {
-                rtns = "{{"+rtn+"}}";
+            if (rtn !== "") {
+                rtns = `$\{${rtn}\}$`;
                 editor.insertHtml(rtns);
             }
             else {
